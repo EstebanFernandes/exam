2060dbb3d4885d177a8acad9178278f526fab63b
Esteban Fernandes <119616775+EstebanFernandes@users.noreply.github.com>
2024-04-17 12:15:32 +0200

    changement du cartbean storebusiness en cours

--- src/main/java/fr/univtours/polytech/exam/dao/ArticleDAOImpl.java
+++ src/main/java/fr/univtours/polytech/exam/dao/ArticleDAOImpl.java
@@ -3,18 +3,30 @@ package fr.univtours.polytech.exam.dao;
 import java.util.List;
 
 import fr.univtours.polytech.exam.model.ArticleBean;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 
+@Stateless
 public class ArticleDAOImpl implements ArticleDAO {
+    @PersistenceContext(unitName = "Exam")
+    private EntityManager em;
 
     public List<ArticleBean> getArticlesList() {
-
-        return null;
+        //On crée une requête afin de récupérer la liste des article présent en BDD
+        Query requete = em.createNativeQuery("select * from ARTICLE", ArticleBean.class);
+        return requete.getResultList();
     }
 
     @Override
     public void updateArticle(ArticleBean article) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateArticle'");
+       
+        em.getTransaction().begin();
+        ArticleBean articleToUpdate = em.find(ArticleBean.class,article.getId());
+        articleToUpdate = article;
+        em.getTransaction().commit();
+        em.close();
     }
 
 }
