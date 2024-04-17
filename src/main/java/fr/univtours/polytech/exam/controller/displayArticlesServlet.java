dcfcea59c69e6f661529f81ede9853ba2aadd554
Esteban Fernandes <119616775+EstebanFernandes@users.noreply.github.com>
2024-04-17 13:53:07 +0200

    comit

--- src/main/java/fr/univtours/polytech/exam/controller/displayArticlesServlet.java
+++ src/main/java/fr/univtours/polytech/exam/controller/displayArticlesServlet.java
@@ -1,8 +1,28 @@
 package fr.univtours.polytech.exam.controller;
 
+import java.io.IOException;
+
+import fr.univtours.polytech.exam.model.CartBean;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "AddNoteServlet", urlPatterns = { "/addNote" })
-public class displayArticlesServlet  {
+@WebServlet(name = "AddNoteServlet", urlPatterns = { "/articles" })
+public class displayArticlesServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        CartBean cart = (CartBean)request.getSession().getAttribute("CART_USER");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("notesList.jsp");
+        dispatcher.forward(request, response);
+    }
 }
--- src/main/java/fr/univtours/polytech/exam/model/CartBean.java
+++ src/main/java/fr/univtours/polytech/exam/model/CartBean.java
@@ -3,14 +3,26 @@ package fr.univtours.polytech.exam.model;
 import java.io.Serializable;
 import java.util.Map;
 
+import fr.univtours.polytech.exam.dao.ArticleDAO;
+import fr.univtours.polytech.exam.dao.ArticleDAOImpl;
+
 public class CartBean implements Serializable {
     // Cette map nous permet d'associer un id d'article à une quantité
-    private Map<Integer, Integer> articlesKeep;
+    private Map<ArticleBean, Integer> articlesKeep;
     // On garde aussi l'id de l'utilisateur
     private UserBean currentUser;
     // Le prix total du panier
     private float TotalPrice;
 
+    public CartBean(){
+        ArticleDAO articles = new ArticleDAOImpl();
+        for(ArticleBean article : articles.getArticlesList())
+        {
+            articlesKeep.put(article,0);
+        }
+        currentUser=null;
+        TotalPrice = 0;
+    }
     public float getTotalPrice() {
         return TotalPrice;
     }
@@ -19,11 +31,11 @@ public class CartBean implements Serializable {
         TotalPrice = totalPrice;
     }
 
-    public Map<Integer, Integer> getArticlesKeep() {
+    public Map<ArticleBean, Integer> getArticlesKeep() {
         return articlesKeep;
     }
 
-    public void setArticlesKeep(Map<Integer, Integer> articlesKeep) {
+    public void setArticlesKeep(Map<ArticleBean, Integer> articlesKeep) {
         this.articlesKeep = articlesKeep;
     }
 
@@ -34,5 +46,5 @@ public class CartBean implements Serializable {
     public void setIdUser(UserBean user) {
         currentUser = user;
     }
-    
+
 }
