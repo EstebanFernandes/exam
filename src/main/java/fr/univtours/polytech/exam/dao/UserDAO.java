15acbce179885ecbc275d99fce10910ca3384f42
nightyday <benabboudouae@gmail.com>
2024-04-17 11:46:28 +0200

    definition methods for ArticleDAO & UserDAO

--- src/main/java/fr/univtours/polytech/exam/dao/UserDAO.java
+++ src/main/java/fr/univtours/polytech/exam/dao/UserDAO.java
@@ -1,5 +1,8 @@
 package fr.univtours.polytech.exam.dao;
 
 public interface UserDAO {
+    // verify if the user is in the database
+    public boolean checkUser(String login, String password);
+
     
 }
--- src/main/java/fr/univtours/polytech/exam/dao/UserDAOImpl.java
+++ src/main/java/fr/univtours/polytech/exam/dao/UserDAOImpl.java
@@ -1,5 +1,12 @@
 package fr.univtours.polytech.exam.dao;
 
+import javax.management.Query;
+
 public class UserDAOImpl implements UserDAO{
+
+    public boolean checkUser(String login, String password){
+    //    Query query = em.createQuery("SELECT u FROM User u WHERE u.login = :login AND u.password = :password");
+        return false;
+    }
     
 }
