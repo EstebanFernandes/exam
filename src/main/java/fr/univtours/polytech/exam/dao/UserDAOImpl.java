9f566557d324038d78a225108b68acd56a066af9
nightyday <benabboudouae@gmail.com>
2024-04-17 12:16:20 +0200

    method verify user

--- src/main/java/fr/univtours/polytech/exam/dao/UserDAOImpl.java
+++ src/main/java/fr/univtours/polytech/exam/dao/UserDAOImpl.java
@@ -1,12 +1,22 @@
 package fr.univtours.polytech.exam.dao;
 
-import javax.management.Query;
+import fr.univtours.polytech.exam.model.UserBean;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 
-public class UserDAOImpl implements UserDAO{
+@Stateless
+public class UserDAOImpl implements UserDAO {
+    @PersistenceContext(unitName = "Exam")
+    private EntityManager em;
 
-    public boolean checkUser(String login, String password){
-    //    Query query = em.createQuery("SELECT u FROM User u WHERE u.login = :login AND u.password = :password");
-        return false;
+    // verify if the user with login and the password exists is in the database
+    public boolean checkUser(String login, String password) {
+        Query requete = em.createNativeQuery("select * from USER_JPA where login = ? and password = ?", UserBean.class);
+        requete.setParameter(1, login);
+        requete.setParameter(2, password);
+        return (requete.getResultList().isEmpty());// if the list is empty, the user is not in the database
     }
-    
+
 }
